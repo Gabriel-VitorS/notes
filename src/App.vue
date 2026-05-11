3d7f5e1492650b1b89778870068b66c4ca3e8634
Gabriel <gabrielvitordasilva@gmail.com>
2026-05-11 13:50:19 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1 class="color-danger-500 border-blue-2">You did it</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <form action="">
+    <label for="">e-mail</label>
+    <input type="text" name="email" id="">
+    <br>
+    <label for="">password</label>
+    <input type="text" class="border-solid border-1 border-blue-1" name="password" id="">
+  </form>
+</template>
+
+<style>
+@import 'tailwindcss';
+</style>
---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnCP56xlq2xGyHZQVfcR8g2CLYqB9A9L4",
+  authDomain: "notes-f5386.firebaseapp.com",
+  projectId: "notes-f5386",
+  storageBucket: "notes-f5386.firebasestorage.app",
+  messagingSenderId: "1046791250425",
+  appId: "1:1046791250425:web:b8427224b32a54a2ca9d28"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
